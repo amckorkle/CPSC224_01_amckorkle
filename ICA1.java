87bf169c60f5d048ef2e2fa2f656aeb02d5752d7
McKorkle Ayla <amckorkle@ada.gonzaga.edu>
2019-01-24 10:38:38 -0800

    functions

--- ICA1.java
+++ ICA1.java
@@ -1,1 +1,33 @@
+/*
+
+*/
+
+public static void main(String[] args){
+	
+}
+
+public static void showKilometers(double meters){
+	double kilometers;
+	
+	kilometers = meters * 0.001;
+}
+
+public static void showInches(double meters){
+	double inches;
+	
+	inches = meters * 39.37;
+}
+
+public static void showFeet(double meters){
+	double feet;
+	
+	kilometers = meters * 3.281;
+}
+
+public static void menu(){
+	System.out.println("1. Convert to Kilometers");
+	System.out.println("2. Convert to Inches");
+	System.out.println("3. Convert to Feet");
+	System.out.println("4. Quit the Program");
+}
 
