89732b6655444e5b4b98e9547b21264bbeda5e5e
k-davis <kaseydavis216@gmail.com>
2019-02-19 14:15:57 -0800

    created basic variables

--- TicTacToe/TicTacToe.java
+++ TicTacToe/TicTacToe.java
@@ -8,6 +8,21 @@
  *
  * @author aylam
  */
-public class TicTacToe {
-    
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class TicTacToe extends JFrame{
+	String p1Name, p2Name;
+	int p1Wins = 0;
+	int p1Losses = 0;
+	int p2Wins = 0;
+	int p2Losses = 0;
+	char[][] board = new char[3][3];
+
+	public static void main(String[] args){
+		
+	}
 }
