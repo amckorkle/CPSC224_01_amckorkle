13b05c5a94f39db348e387f39f90e5efc943b2d9
Kasey Davis <kaseydavis216@gmail.com>
2019-02-21 20:51:53 -0800

    Added image icons

--- TicTacToe/TicTacToe.java
+++ TicTacToe/TicTacToe.java
@@ -15,167 +15,151 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+public class TicTacToe extends JFrame {
+    /*
+     * To-do: Do we need a UML diagram?
+     */
+    Player player1 = new Player("icons/X.png");
+    Player player2 = new Player("icons/O.png");
+    Player[][] board = new Player[3][3];
+    private final int WINDOW_WIDTH = 500;
+    private final int WINDOW_HEIGHT = 500;
+    private GridPanel gameBoard;
+    private GameInfoPanel gameLabel;
+    private buildButtonPanel exitButton;
+    // private JTextField player1;
+    // private JTextField player2;
+    private JPanel centerPanel;
+    private BuildPlayerStatsPanel player1Panel;
 
-public class TicTacToe extends JFrame{
-	/*
-	To-do:
-	Do we need a UML diagram?
-	*/
-	Player player1 = new Player('X');
-    Player player2 = new Player('O');
-	char[][] board = new char[3][3];
-	private final int WINDOW_WIDTH = 500;
-	private final int WINDOW_HEIGHT = 500;
-	private GridPanel gameBoard;
-	private GameInfoPanel gameLabel;
-        private buildButtonPanel exitButton;
-	//private JTextField player1;
-	//private JTextField player2;
-        private JPanel centerPanel;
-        private BuildPlayerStatsPanel player1Panel;
+    public TicTacToe() {
+        setTitle("Tic Tac Toe");
 
-        
-	public TicTacToe(){
-		setTitle("Tic Tac Toe");
-		
-		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-                setLayout(new BorderLayout());
-                
-                centerPanel = new JPanel(new BorderLayout());
-                
-                gameLabel = new GameInfoPanel();
-                gameBoard = new GridPanel();
-                exitButton = new buildButtonPanel();
-                player1Panel = new BuildPlayerStatsPanel();
-		
-                centerPanel.add(gameBoard, BorderLayout.CENTER);
-                centerPanel.add(exitButton, BorderLayout.SOUTH);
-                
-                add(centerPanel, BorderLayout.CENTER);
-                add(gameLabel, BorderLayout.SOUTH);
-                add(player1Panel, BorderLayout.NORTH);
-                
-                
-                //pack();
-		
-		setVisible(true);
-			
-	}
-        
-        
-        private void buildGridPanel() {
-            
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLayout(new BorderLayout());
+
+        centerPanel = new JPanel(new BorderLayout());
+
+        gameLabel = new GameInfoPanel();
+        gameBoard = new GridPanel();
+        exitButton = new buildButtonPanel();
+        player1Panel = new BuildPlayerStatsPanel();
+
+        centerPanel.add(gameBoard, BorderLayout.CENTER);
+        centerPanel.add(exitButton, BorderLayout.SOUTH);
+
+        add(centerPanel, BorderLayout.CENTER);
+        add(gameLabel, BorderLayout.SOUTH);
+        add(player1Panel, BorderLayout.NORTH);
+
+        // pack();
+
+        setVisible(true);
+
+    }
+
+    private void buildGridPanel() {
+
+    }
+
+    public static void main(String[] args) {
+        new TicTacToe();
+    }
+
+    private Player winnerExists() {
+        // Check horizontals
+        for (int row = 0; row < board.length; row++) {
+            int idx = 1;
+            while (board[row][0] != null && board[row][0] == board[row][idx] && idx < 3) {
+                if (idx == 2) {
+                    return board[row][0];
+                }
+                idx++;
+            }
         }
- 
-	public static void main(String[] args){
-		new TicTacToe();
-	}
 
-    
+        // Check verticals
+        for (int col = 0; col < board.length; col++) {
+            int idx = 1;
+            while (board[0][col] != null && board[0][col] == board[idx][col] && idx < 3) {
+                if (idx == 2) {
+                    return board[0][col];
+                }
+                idx++;
+            }
+        }
 
-	private Player winnerExists(){
-		// Check horizontals
-		for(int row = 0; row < board.length; row++){
-			int idx = 1;
-			while(board[row][0] != '\0' && board[row][0] == board[row][idx] && idx < 3){
-				if(idx == 2){
-					return getPlayerFromSymbol(board[row][0]);
-				}
-				idx++;
-			}
-		}
-		
-		// Check verticals
-		for(int col = 0; col < board.length; col++){
-			int idx = 1;
-			while(board[0][col] != '\0' && board[0][col] == board[idx][col] && idx < 3){
-				if(idx == 2){
-					return getPlayerFromSymbol(board[0][col]);
-				}
-				idx++;
-			}
-		}
+        // Check diagonals
+        int idx = 1;
+        while (board[0][0] != null && board[0][0] == board[idx][idx] && idx < 3) {
+            if (idx == 2) {
+                return board[0][0];
+            }
+            idx++;
+        }
 
-		// Check diagonals
-		int idx = 1;
-		while(board[0][0] != '\0' && board[0][0] == board[idx][idx] && idx < 3){
-			if(idx == 2){
-				return getPlayerFromSymbol(board[0][0]);
-			}
-			idx++;
-		}
+        while (board[2][2] != null && board[2][2] == board[2 - idx][2 - idx] && idx < 3) {
+            if (idx == 2) {
+                return board[2][2];
+            }
+            idx++;
+        }
 
-		while(board[2][2] != '\0' && board[2][2] == board[2-idx][2-idx] && idx < 3){
-			if(idx == 2){
-				return getPlayerFromSymbol(board[2][2]);
-			}
-			idx++;
-		}
+        return null;
+    }
 
-		return null;
-	}
+    private void clearBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; i++) {
+                board[i][j] = null;
+            }
+        }
+    }
 
-	private Player getPlayerFromSymbol(char symbol){
-		if(symbol == player1.symbol){
-			return player1;
-		} else {
-			return player2;
-		}
-	}
+    private void placeSymbol(int col, int row, Player curPlayer) {
+        board[row][col] = curPlayer;
+    }
 
-	private void clearBoard(){
-		for(int i = 0; i < 3; i++){
-			for(int j = 0; j < 3; i++){
-				board[i][j] = '\0';
-			}
-		}
-	}
-
-	private void placeSymbol(int col, int row, char symbol){
-		board[row][col] = symbol;
-	}
-
-	private class Player{
-        private char symbol;
+    private class Player {
+        private ImageIcon symbol;
         private int wins = 0;
         private int losses = 0;
         private String name;
 
-        public Player(char Symbol){
-            symbol = Symbol;
+        public Player(String path) {
+            symbol = new ImageIcon(path);
         }
 
-        public void setName(String newName){
+        public void setName(String newName) {
             name = newName;
         }
 
-        public String getName(){
+        public String getName() {
             return name;
         }
 
-        public void incrementWins(){
+        public void incrementWins() {
             wins++;
         }
 
-        public int getWins(){
+        public int getWins() {
             return wins;
         }
 
-        public void incrementLosses(){
+        public void incrementLosses() {
             losses++;
         }
 
-        public int getLosses(){
+        public int getLosses() {
             return losses;
         }
 
-        public char getSymbol(){
+        public ImageIcon getSymbolIcon() {
             return symbol;
         }
 
     }
 }
-
