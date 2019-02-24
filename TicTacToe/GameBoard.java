0eaef395f4de3149cd7a3396383b3c33154494a0
Kasey Davis <kaseydavis216@gmail.com>
2019-02-24 13:57:08 -0800

    Fixed win detection bug

--- TicTacToe/GameBoard.java
+++ TicTacToe/GameBoard.java
@@ -12,69 +12,71 @@ public class GameBoard {
 	private Player[][] board;
 	private Player[] playerList;
 	private GridPanel gridPanel;
-	private GameInfoPanel gameInfoPanel;	
+	private GameInfoPanel gameInfoPanel;
 	private int nextPlayerTurn = 0;
 	private int turnCount = 0;
 
 	/*
-    * consturctor
-    */
-	public GameBoard(Player[] players){
+	 * consturctor
+	 */
+	public GameBoard(Player[] players) {
 		board = new Player[3][3];
 		playerList = players;
-		
+
 	}
 
 	/**
-      assignGridPanel method
-      @param gridPanel is the gridPanel used to play
-   */
-	public void assignGridPanel(GridPanel gridPanel){
+	 * assignGridPanel method
+	 * 
+	 * @param gridPanel is the gridPanel used to play
+	 */
+	public void assignGridPanel(GridPanel gridPanel) {
 		this.gridPanel = gridPanel;
 	}
 
 	/**
-      assignGameInfoPanel method
-      @param panel is the panel that holds the game info
-   */
-	public void assignGameInfoPanel(GameInfoPanel panel){
+	 * assignGameInfoPanel method
+	 * 
+	 * @param panel is the panel that holds the game info
+	 */
+	public void assignGameInfoPanel(GameInfoPanel panel) {
 		gameInfoPanel = panel;
 	}
 
 	/**
-      enableGrid method
-      enables all of the buttons in the grid to be pushed
-   */
-	public void enableGrid(){
+	 * enableGrid method enables all of the buttons in the grid to be pushed
+	 */
+	public void enableGrid() {
 		gameInfoPanel.setInfoPanelText(playerList[nextPlayerTurn].getName() + "'s turn.");
-		
-		for(int i = 0; i < 3; i++){
-			for(int j = 0; j < 3; j++){
-          		gridPanel.buttonGrid[i][j].setEnabled(true);
-        	}
-    	}
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				gridPanel.buttonGrid[i][j].setEnabled(true);
+			}
+		}
 	}
 
 	/**
-      disableGrid method
-      disables all of the buttons in the grid so they cannot be pushed
-   */
-	public void disableGrid(){
-		for(int i = 0; i < 3; i++){
-			for(int j = 0; j < 3; j++){
+	 * disableGrid method disables all of the buttons in the grid so they cannot be
+	 * pushed
+	 */
+	public void disableGrid() {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
 				gridPanel.buttonGrid[i][j].setEnabled(false);
 			}
 		}
 	}
 
 	/**
-      getIconAt method
-      @param row the row that button is in
-	  @param col the column the button is in
-	  @return the symbol that is on that button or otherwise null
-   */
-	public ImageIcon getIconAt(int row, int col){
-		if(playerAt(row, col) != null){
+	 * getIconAt method
+	 * 
+	 * @param row the row that button is in
+	 * @param col the column the button is in
+	 * @return the symbol that is on that button or otherwise null
+	 */
+	public ImageIcon getIconAt(int row, int col) {
+		if (playerAt(row, col) != null) {
 			return playerAt(row, col).getSymbolIcon();
 		}
 
@@ -82,30 +84,31 @@ public class GameBoard {
 	}
 
 	/**
-      handleGameplayEvent method
-      @param row the row that button is in
-	  @param col the column the button is in
-   */
-	public void handleGameplayEvent(int row, int col){
+	 * handleGameplayEvent method
+	 * 
+	 * @param row the row that button is in
+	 * @param col the column the button is in
+	 */
+	public void handleGameplayEvent(int row, int col) {
 		place(row, col, playerList[nextPlayerTurn]);
 		incrementNextPlayerTurn();
 		turnCount++;
 
 		Player winner = winnerExists();
-		if(winner != null){
+		if (winner != null) {
 			gameInfoPanel.setInfoPanelText(winner.getName() + " won!");
 			disableGrid();
 			winner.incrementWins();
-			for(Player player : playerList){
-				if(player != winner){
+			for (Player player : playerList) {
+				if (player != winner) {
 					player.incrementLosses();
 				}
 			}
 
-		} else if(turnCount >= 9){
+		} else if (turnCount >= 9) {
 			gameInfoPanel.setInfoPanelText("Cat's game--no one wins.");
 			disableGrid();
-			for(Player player : playerList){
+			for (Player player : playerList) {
 				player.incrementLosses();
 			}
 
@@ -116,36 +119,39 @@ public class GameBoard {
 	}
 
 	/**
-      playerAt method
-      @param row the row that button is in
-	  @param col the column the button is in
-	  @return the button that is at that row and column
-   */
-	public Player playerAt(int row, int col){
+	 * playerAt method
+	 * 
+	 * @param row the row that button is in
+	 * @param col the column the button is in
+	 * @return the button that is at that row and column
+	 */
+	public Player playerAt(int row, int col) {
 		return board[row][col];
-	}	
+	}
 
 	/**
-      place method
-      @param row the row that button is in
-	  @param col the column the button is in
-	  @param player the player that has the turn
-   */
-	public void place(int row, int col, Player player){
+	 * place method
+	 * 
+	 * @param row    the row that button is in
+	 * @param col    the column the button is in
+	 * @param player the player that has the turn
+	 */
+	public void place(int row, int col, Player player) {
 		board[row][col] = player;
 	}
 
 	/**
-      playerAt method
-      @param isReset true if we want the board disabled otherwise false
-   */
-	public void reset(boolean isReset){
+	 * playerAt method
+	 * 
+	 * @param isReset true if we want the board disabled otherwise false
+	 */
+	public void reset(boolean isReset) {
 		for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = null;
-				if(isReset)
+			for (int j = 0; j < 3; j++) {
+				board[i][j] = null;
+				if (isReset)
 					gridPanel.buttonGrid[i][j].setEnabled(false);
-            }
+			}
 		}
 
 		nextPlayerTurn = 0;
@@ -155,67 +161,72 @@ public class GameBoard {
 	}
 
 	/**
-      incrementNewPlayerTurn method
-   */
-	private void incrementNextPlayerTurn(){
+	 * incrementNewPlayerTurn method
+	 */
+	private void incrementNextPlayerTurn() {
 		nextPlayerTurn++;
 		nextPlayerTurn %= playerList.length;
 	}
 
 	/**
-      isPlayerAt method
-      @param row the row that button is in
-	  @param col the column the button is in
-	  @return the button that is at that row and column is null
-   */
-	public boolean isPlayerAt(int row, int col){
+	 * isPlayerAt method
+	 * 
+	 * @param row the row that button is in
+	 * @param col the column the button is in
+	 * @return the button that is at that row and column is null
+	 */
+	public boolean isPlayerAt(int row, int col) {
 		return board[row][col] != null;
 	}
 
 	/**
-      winnerExists method
-	  @return the final location to check if the user has won or not
-   */
+	 * winnerExists method
+	 * 
+	 * @return the final location to check if the user has won or not
+	 */
 	public Player winnerExists() {
-        // Check horizontals
-        for (int row = 0; row < 3; row++) {
-            int idx = 1;
-            while (board[row][0] != null && board[row][0] == board[row][idx] && idx < 3) {
-                if (idx == 2) {
-                    return board[row][0];
-                }
-                idx++;
-            }
-        }
+		// Check horizontals
+		int idx;
+		for (int row = 0; row < 3; row++) {
+			idx = 1;
+			while (board[row][0] != null && board[row][0] == board[row][idx] && idx < 3) {
+				if (idx == 2) {
+					return board[row][0];
+				}
+				idx++;
+			}
+		}
 
-        // Check verticals
-        for (int col = 0; col < board.length; col++) {
-            int idx = 1;
-            while (board[0][col] != null && board[0][col] == board[idx][col] && idx < 3) {
-                if (idx == 2) {
-                    return board[0][col];
-                }
-                idx++;
-            }
-        }
+		// Check verticals
+		for (int col = 0; col < board.length; col++) {
+			idx = 1;
+			while (board[0][col] != null && board[0][col] == board[idx][col] && idx < 3) {
+				if (idx == 2) {
+					return board[0][col];
+				}
+				idx++;
+			}
+		}
 
-        // Check diagonals
-        int idx = 1;
-        while (board[0][0] != null && board[0][0] == board[idx][idx] && idx < 3) {
-            if (idx == 2) {
-                return board[0][0];
-            }
-            idx++;
-        }
+		// Check diagonals
+		idx = 1;
+		while (board[0][0] != null && board[0][0] == board[idx][idx] && idx < 3) {
 
-        while (board[0][2] != null && board[0][2] == board[idx][2 - idx] && idx < 3) {
-            if (idx == 2) {
-                return board[0][2];
-            }
-            idx++;
-        }
+			if (idx == 2) {
+				return board[0][0];
+			}
+			idx++;
+		}
 
-        return null;
-    }
+		idx = 1;
+		while (board[0][2] != null && board[0][2] == board[idx][2 - idx] && idx < 3) {
+			if (idx == 2) {
+				return board[0][2];
+			}
+			idx++;
+		}
+
+		return null;
+	}
 
 }
