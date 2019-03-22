b858be6b08c98a7b97c13f8b618ce78757601e2e
k-davis <kaseydavis216@gmail.com>
2019-03-22 16:05:04 -0700

    Defined parallax function

--- MotionParallax/MotionParallax.java
+++ MotionParallax/MotionParallax.java
@@ -3,11 +3,22 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class MotionParallax extends JFrame {
+	Timer timer;
+	Point mousePos;
+
 	public MotionParallax(){
+		timer = new Timer(30, new timerListener());
+		mousePos = new Point();
+		addMouseMotionListener(new MyMouseMotionListener());
+		
 		setTitle("Motion Parallax");
 		setSize(500, 500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		timer.start();
 		setVisible(true);
+
+
 	}
 
 	public static void main(String[] args){
@@ -15,7 +26,32 @@ public class MotionParallax extends JFrame {
 	}
 
     public void paint(Graphics g){
-        super.paint(g);
+		super.paint(g);
         g.drawOval(250, 250, 20, 20);
-    }
+	}
+	
+	public Point computeParalaxPos(Point objPos, Point mousePos, double parallaxAmt){
+		int x = objX + (int)(parallaxAmt * (mousePos.x - objPos.x));
+		int y = objY + (int)(parallaxAmt * (mousePos.Y - objPos.y));
+		return new Point(x, y);
+	}
+
+	private class timerListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			repaint();
+		}
+	}
+
+	private class MyMouseMotionListener implements MouseMotionListener{
+		public void mouseDragged(MouseEvent e)
+        {
+        }
+      
+        
+        public void mouseMoved(MouseEvent e)
+        {
+			mousePos.setLocation(e.getX(), e.getY());
+		}
+		
+	}
 }
