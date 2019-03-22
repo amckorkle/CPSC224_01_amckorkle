8bc173851894b0f0cb27a6b6c87e2f2375c9748e
amckorkle <amckorkle@zagmail.gonzaga.edu>
2019-03-22 16:10:43 -0700

    added images

--- MotionParallax/MotionParallax.java
+++ MotionParallax/MotionParallax.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
 
 public class MotionParallax extends JFrame {
 	public MotionParallax(){
@@ -10,12 +11,55 @@ public class MotionParallax extends JFrame {
 		setVisible(true);
 	}
 
+    public void paint(Graphics g){
+        super.paint(g);
+        drawBackgroundMountain(g, .5);
+        drawForegroundMountains(g, .7);
+        drawSun(g, 0);  
+        drawRiver(g, 1);  
+    }
+
+    public void drawSun(Graphics g, double layer){
+        g.setColor(Color.YELLOW);
+        g.drawOval(350, 100, 100, 100);
+        g.fillOval(350, 100, 100, 100);
+    }
+
+    public void drawRiver(Graphics g, double layer){
+        g.setColor(Color.BLUE);
+        g.drawRect(0, 400, 500, 100);
+        g.fillRect(0, 400, 500, 100);
+    }
+
+    public void drawBackgroundMountain(Graphics g, double layer){
+        int xValues[] = {100, 300, 500};
+        int yValues[] = {500, 200, 500};
+        Random rand = new Random();
+        int red = rand.nextInt(256);
+        int green = rand.nextInt(256);
+        int blue = rand.nextInt(256);
+
+        Color randomColor = new Color(red,green,blue);
+
+        g.setColor(randomColor);
+        g.fillPolygon(xValues, yValues, 3);
+    }
+
+    public void drawForegroundMountains(Graphics g, double layer){
+        int xValues[] = {-50, 130, 310, 440, 570};
+        int yValues[] = {500, 150, 500, 260, 500};
+        Random rand = new Random();
+        int red = rand.nextInt(256);
+        int green = rand.nextInt(256);
+        int blue = rand.nextInt(256);
+
+        Color randomColor = new Color(red,green,blue);
+
+        g.setColor(randomColor);
+        g.fillPolygon(xValues, yValues, 5);
+    }
+
 	public static void main(String[] args){
 		new MotionParallax();
 	}
-
-    public void paint(Graphics g){
-        super.paint(g);
-        g.drawOval(250, 250, 20, 20);
-    }
 }
